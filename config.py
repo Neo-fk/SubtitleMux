4ddf74c31facfb013dc9f3e13d0e96e978db1d22
Anuwa <81525953+Neo-fk@users.noreply.github.com>
2023-09-24 21:19:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,12 @@
-
 import os
 
 class Config:
 
-    BOT_TOKEN = "5410320498:AAGAPfA_Wnl4ZQRpCIWhxQR008O97_aIf7I"
-    APP_ID = 10247139
-    API_HASH = "96b46175824223a33737657ab943fd6a"
+    BOT_TOKEN = os.environ.get('5800802984:AAG0EUk9edno0k83LcHcnfwT1FtPtdjdQRk', None)
+    APP_ID = os.environ.get('22404062', None)
+    API_HASH = os.environ.get('162d4eb8c9cbfe558121c6cf41f8df43', None)
 
     #comma seperated user id of users who are allowed to use
-    ALLOWED_USERS = [x.strip(' ') for x in ('1098504493,1443454117').split(',')]
+    ALLOWED_USERS = [x.strip(' ') for x in os.environ.get('ALLOWED_USERS','1165215979').split(',')]
 
     DOWNLOAD_DIR = 'downloads'
